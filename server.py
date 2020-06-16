7af99806c425f80d7605bb4afe9547cb98e1bcc2
allen44 <allen44@users.noreply.github.com>
2020-06-15 18:53:33 -1000

    Updated the contact form to handle form data from client

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_from_directory, url_for
+from flask import Flask, render_template, send_from_directory, url_for, request, redirect
+
 app = Flask(__name__)
 print(__name__)
 
@@ -6,23 +7,15 @@ print(__name__)
 def root():
     return render_template('index.html')
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
-    
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou.html')
+    else:
+        return 'something went wrong, try again.'
